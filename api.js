5d74fb13f9badea10b32a111b1faf5339f2298e5
shcox <45539562+shcox@users.noreply.github.com>
2020-11-03 13:31:21 -0500

    Creating fake data-->using the same basic format for Faker as Alex. Thanks Alex.

---
+++ api.js
@@ -0,0 +1,30 @@
+import pkg from "faker";
+import {createServer} from 'http';
+import {parse} from 'url';
+import {join} from 'path';
+import {writeFile, readFileSync, existsSync} from 'fs';
+const {name,internet,company,address,lorem,commerce} = pkg;
+
+'use strict';
+
+function createFakePet() {
+    //Pet Objects: Pet Name, Breed, About, Health, Location, Comments, Num Likes
+    let i;
+    let commarr = [];
+    const num_com = Math.random() * 10
+    for (i = 0; i < num_com; i++) {
+        commarr.push({comment: lorem.sentence(), user: internet.userName()});
+    }
+    
+    const pet = {
+        name:  name.firstName(),
+        breed: 'terrier',
+        about: lorem.paragraph(),
+        health: lorem.paragraph(),
+        location: address.city(),
+        comments: commarr,
+        num_likes: Math.random() * 100
+    };
+    return pet;
+}
+
