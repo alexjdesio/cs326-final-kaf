dee99ed71c0817553a8a9282a5416ca0eb8d0972
joehuang1234 <58898873+joehuang1234@users.noreply.github.com>
2020-11-14 13:16:06 -0500

    Reset

--- api.js
+++ api.js
@@ -1,19 +1,194 @@
-import pkg from "faker";
-import express from "express";
-import * as bodyParser from "body-parser";
-const {name,internet,company,address,lorem,commerce,image} = pkg;
 
+import pkg from 'faker';
+import {createServer} from 'http';
+import {parse} from 'url';
+//const bodyParser = require('body-parser');
+import express from "express";
+
+//import {join} from 'path';
+//import {writeFile, readFileSync, existsSync} from 'fs';
+
+const {name,internet,company,address,lorem,commerce,image} = pkg;
 
 'use strict';
 
+function createFakeUser(username){
+    let interestIndex = Math.floor((Math.random()*3))
+    let interestArray = ["dog","cats","exotics"];
+    let userType = (Math.random() > 0.5) ? "adopter" : "shelter";
+    
+    let user = {
+        username: username,
+        email: internet.email(), 
+        password: internet.password(),
+        type: userType,
+        interests: (userType === "adopter") ? interestArray[interestIndex] : "null",
+        shelter: (userType === "shelter") ? company.companyName() : "null",
+        liked_pets: [],
+        viewed_pets: [],
+        location: address.city(),
+        num_likes: 0
+    };
+    //console.log(user);
+    return user;
+}
+
+function createFakeLogin(){
+    let login = {
+        username: internet.userName(),
+        password: internet.password(), 
+    };
+    return login;
+}
+
+function createFakeLoginToken(){
+    return internet.password(); //using this as a placeholder
+}
+
+function createFakePetResult(type,query){
+    let pet = {
+        pet_name: name.firstName(),
+        pet_location: company.companyName(),
+        pet_breed: commerce.color(),
+        pet_about: lorem.sentence(5,10),
+        pet_health: lorem.sentence(5,10),
+        pet_comments: [],
+        picture: image.cats(),
+        num_likes: 0
+    };
+    let fields = Object.keys(pet);
+    if(fields.includes(type)){ //guarantees that the fake data satisfies the search constraints
+        pet[type] = query;
+    }
+    return pet;
+}
+
+function createFakeSearchResult(type,query,quantity){
+    let shelter_fields = Object.keys(createFakeShelterResult("null",""));
+    let pet_fields = Object.keys(createFakePetResult("null",""));
+    let returnArr = [];
+
+    if(pet_fields.includes(type)){
+        for(let i = 0;i<parseInt(quantity);i++){
+            returnArr.push(createFakePetResult(type,query));
+        }
+    }
+    else if(shelter_fields.includes(type)){
+        for(let i = 0;i<parseInt(quantity);i++){
+            returnArr.push(createFakeShelterResult(type,query));
+        }
+    }
+    else{
+        console.log("Invalid query type");
+    }
+    return returnArr;
+}
+
+//API
+let database = {
+    users: [],
+    tokens: []
+};
+
+database.users.push(createFakeUser("Eric")); //used for testing user/id/edit
+	
+function createFakePet(pet_name) {
+    //Pet Objects: Name, Breed, About, Health, Location, Comments, Num Likes
+    let i;
+    const commarr = [];
+    const num_com = Math.random() * 10;
+    for (i = 0; i < num_com; i++) {
+        commarr.push({comment: lorem.sentence(), user: internet.userName()});
+    }
+    
+    const pet = {
+        name:  pet_name,
+        breed: 'terrier',
+        about: lorem.paragraph(),
+        health: lorem.paragraph(),
+        location: company.companyName(),
+        comments: commarr,
+        num_likes: Math.floor(Math.random() * 100),
+        picture: image.cats()
+    };
+    return pet;
+}
+
+function createFakeShelter(shelter_name) {
+    //Shelter Objects: Shelter Name, About, Pets, Location, Comments, Num Likes
+    let i;
+    const commarr = [];
+    const num_com = Math.random() * 10;
+    for (i = 0; i < num_com; i++) {
+        commarr.push({comment: lorem.sentence(), user: internet.userName()});
+    }
+
+    const petarr = [];
+    const num_pets = Math.random() * 10;
+    for (i = 0; i < num_pets; i++) {
+        petarr.push(createFakePet(name.firstName()));
+    }
+
+    const shelter = {
+        name: shelter_name,
+        about: lorem.paragraph(),
+        pets: petarr,
+        location: address.city(),
+        comments: commarr,
+        num_likes: Math.floor(Math.random() * 100),
+        picture: image.business()
+    };
+
+    return shelter;
+}
+
+function favoriteShelters(range) {
+    const shelters = [];
+    let i;
+    for (i = 0; i < range; i++) {
+        shelters.push(createFakeShelter(company.companyName()));
+    }
+    return shelters;
+}
+
+function recentlyViewedPets() {
+    const pets = [];
+    let i;
+    for (i = 0; i < 5; i++) {
+        pets.push(createFakePet(name.firstName()));
+    }
+    return pets;
+}
+
+function favoritePets(range) {
+    const pets = [];
+    let i;
+    for (i = 0; i < range; i++) {
+        pets.push(createFakePet(name.firstName()));
+    }
+    return pets;
+}
+
 //EXPERIMENTING WITH EXPRESS.JS
 const app = express(); // this is the "app"
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080;     // we will listen on this port
 app.use(express.json({type: ['application/json', 'text/plain']})); 
 
 app.listen(port, () => {
-      console.log('App listening at http://localhost:${port}');
+  console.log('App listening at http://localhost:${port}');
 });
+
+app.use('/',express.static('./html')); //Serves static pages(index.html, search.html, etc.)
+
+app.get('/search',express.urlencoded(),search);
+
+app.get('/user/id/view',express.json(), (req,res) => res.end(JSON.stringify(createFakeUser(req.query.username))));
+
+app.post("/register",express.json(), (req,res) => res.end("Registration Successful."));
+
+app.post("/login",express.json(),login); //should be POST, works when set to GET
+
+app.post("/user/id/edit",express.json(),userEdit);
 
 app.use('/',express.static('./html')); //Serves static pages(index.html, search.html, etc.)
 
@@ -31,7 +206,7 @@ app.post('/chat/msg', (req, res) => {
     res.send('Success');});
 
 //Shelter Page
-app.get('/shelter/view', bodyParser.json(), (req, res) => res.end(JSON.stringify(createFakeShelterResult(null, null))));
+app.get('/shelter/view', (req, res) => res.end(JSON.stringify(createFakeShelterResult(null, null))));
 app.post('/shelter/edit', (req, res) => {
     console.log(req.body);
     res.send('Success');}); 
@@ -86,25 +261,55 @@ function createFakeShelterResult(type,query){
     return shelter;
 }
 
-function userEdit(req,res){  
-    //let options = req.body; 
-    //console.log("Edit request, Body:",JSON.stringify(options));  
-    res.send("Request received successfully.");  
+//Handles search requests and returns search results (with fake data)
+function search(req,res){
+    let options = req.query;
+    res.end(JSON.stringify(createFakeSearchResult(options.type,options.query,options.quantity)));
 }
 
-function createFakePetResult(type,query){
-    let pet = {
-        pet_name: name.firstName(),
-        pet_location: company.companyName(),
-        pet_breed: commerce.color(),
-        pet_about: lorem.sentence(5,10),
-        pet_health: lorem.sentence(5,10),
-        pet_comments: [],
-        picture: image.cats()
-    };
-    let fields = Object.keys(pet);
-    if(fields.includes(type)){ //guarantees that the fake data satisfies the search constraints
-        pet[type] = query;
+//Handles login requests and returns a fake session token(will be replaced in future milestones.)
+function login(req, res){
+    let options = req.body;
+    console.log(options);
+    if((options.username === null) || (options.password === null)){
+        //validates that sufficient information was passed in for the login attempt to occur
+        res.end("Login attempt failed- Invalid request.");
+        return;
     }
-    return pet;
+    else if(Math.random() > 0.5){ //50% Chance of success on login to simulate successful or unsuccessful login attempts
+        //if login succeeds
+        let newToken = createFakeLoginToken();
+        database.tokens.push(newToken);
+        res.end(JSON.stringify(newToken));
+    }
+    else{
+        //if login failed
+        res.end("Login attempt failed- Username and/or Password incorrect.");
+    }
 }
+
+//Edits user data stored on the server.
+// some fields not successfully received
+function userEdit(req,res){
+    let options = req.body;
+    console.log("Edit request, Body:",JSON.stringify(options));
+    res.end("Request received successfully.");   
+}
+
+app.get('/pet/view',express.json(), (req,res) => res.end(JSON.stringify(createFakePet(req.query.name))));
+
+app.get('/shelter/view',express.json(), (req,res) => res.end(JSON.stringify(createFakeShelter(req.query.name))));
+
+app.get('/user/id/favoritepets/view',express.json(), (req,res) => res.end(JSON.stringify(favoritePets(req.query.range))));
+
+app.get('/user/id/favoriteshelters/view',express.json(), (req,res) => res.end(JSON.stringify(favoriteShelters(req.query.range))));
+
+app.get('/user/id/recentlyviewedpets',express.json(), (req,res) => res.end(JSON.stringify(recentlyViewedPets())));
+
+app.post("/pet/comments/create",express.json(), (req,res) => res.end("Comment Recieved"));
+
+app.post("/user/id/favoritepets/add",express.json(), (req,res) => res.end("Added Pet to Favorites"));
+
+app.post("/user/id/favoritepets/delete",express.json(), (req,res) => res.end("Removed Pet from Favorites"));
+
+app.post("/pet/create",express.json(), (req,res) => res.end("Info Recieved."));
