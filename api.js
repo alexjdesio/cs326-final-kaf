85dab5f3bd56c006d2179a0251e5404fce5e9c39
joehuang1234 <58898873+joehuang1234@users.noreply.github.com>
2020-11-20 10:06:37 -0500

    Fixed some endpoints

--- api.js
+++ api.js
@@ -34,7 +34,6 @@ app.use(express.json());
 app.use(express.static('client'));
 app.listen(port, () => {
     console.log('App listening at http://localhost:${port}');
-<<<<<<< HEAD
 });
  
 //Start of Endpoints
@@ -51,7 +50,6 @@ app.use(express.static('html'));
 
 //Alex's MongoDB endpoints:
 const strategy = new LocalStrategy(async (username, password, done) => {
-    let database = client.db('petIt');
     //Check if the user exists  
     if (!findUser(username)) {
         // If no such user
@@ -66,9 +64,9 @@ const strategy = new LocalStrategy(async (username, password, done) => {
         console.log("user does not exist");
         return done(null, false, { 'message' : 'User does not exist' });
     }
-
+    console.log(result);
     //Check if the password matches what is stored in the database for the given salt
-    if(mc.check(password,result.salt,result.password)){
+    if(mc.check(password, result.salt, result.password)){
         console.log("login succeeded");
         return done(null, username);
     }
@@ -77,7 +75,7 @@ const strategy = new LocalStrategy(async (username, password, done) => {
         await new Promise((r) => setTimeout(r, 2000)); // two second delay
         return done(null, false, { 'message' : 'Wrong password' });
     }
-    });
+});
 
 // App configuration
 
@@ -107,7 +105,6 @@ app.use(express.urlencoded({'extended' : true})); // allow URLencoded data
 
 // Returns true if the user exists.
 async function findUser(username) {
-    let database = client.db('petIt');
     //check if the username is in the database
     let check_query = {"username": username};
     let result = await database.collection("users").findOne(check_query); // do I need to await these calls?
@@ -121,7 +118,6 @@ async function findUser(username) {
 
 // Returns true if the password matches the one stored in the database.
 async function validatePassword(username, password) {
-    let database = client.db('petIt');
     //Make sure this is a valid search request
     //Get the user associated with the username
     let check_query = {"username": username};
@@ -195,7 +191,6 @@ app.get('/register', (req, res) => res.sendFile('html/register.html',{ 'root' : 
 // Use req.body to access data (as in, req.body['username']).
 // Use res.redirect to change URLs.
 app.post("/register",express.json(), async (req,res) => {
-    let database = client.db('petIt');
     //check if the username is in the database
     let check_query = {"username": req.body.username};
     let result = await database.collection("users").findOne(check_query); // do I need to await these calls?
@@ -234,43 +229,8 @@ app.post("/register",express.json(), async (req,res) => {
     res.redirect('/login');
 });
 
-/** 
-//TODO: Finish this function
-app.post("/login",express.json(),async (req,res) => {
-    let database = client.db('petIt');
-    //Make sure this is a valid search request
-    let requiredFields = {
-        username: null,
-        password: null
-    };
-    for(let field of Object.keys(requiredFields)){
-        if(req.body[field] === null){ 
-            res.end("Invalid request- missing username or password."); 
-            return;
-        }
-    }
-    //Get the user associated with the username
-    let check_query = {"username": req.body.username};
-    let result = await database.collection("users").findOne(check_query); // do I need to await these calls?
-    if(result === null){
-        console.log("User does not exist.");
-        res.end("User does not exist.");
-        return;
-    }
-    //Check if the password matches what is stored in the database for the given salt
-    if(mc.check(req.body.password,result.salt,result.password)){
-        //give the user an authentication token
-        res.end("Successfully logged in");
-    }
-    else{
-        res.end("Login failed.");
-    }
-});
-**/
-
 //Needs testing
 app.post("/user/id/edit",express.json(), async (req,res) =>{
-    let database = client.db("petIt");
     //check if the username is in the database
     let check_query = {"username": req.body.username};
     let result = await database.collection("users").findOne(check_query); // do I need to await these calls?
@@ -300,7 +260,6 @@ app.post("/user/id/edit",express.json(), async (req,res) =>{
 });
 
 app.get('/search',express.urlencoded(), async (req,res) => {
-    let database = client.db('petIt');
     //Make sure this is a valid search request
     let requiredFields = {
         type: null,
@@ -355,7 +314,6 @@ app.get('/search',express.urlencoded(), async (req,res) => {
 
 //TODO: filter what's returned so that the password + salt are not returned
 app.get('/user/id/view',express.json(), async (req,res) => {
-    let database = client.db('petIt');
     let query = {"username": req.body.username};
     let result = await database.collection("users").findOne(query); // do I need to await these calls?
     res.end(JSON.stringify(result));
@@ -476,232 +434,6 @@ async function getID(type){
 }
 
 //*********************************************************************************************************************
-function createFakeUser(username){
-    let interestIndex = Math.floor((Math.random()*3));
-    let interestArray = ["dog","cats","exotics"];
-    let userType = (Math.random() > 0.5) ? "adopter" : "shelter";
-    let user = {
-        username: username,
-        email: internet.email(), 
-        password: internet.password(),
-        type: userType,
-        interests: (userType === "adopter") ? interestArray[interestIndex] : "null",
-        shelter: (userType === "shelter") ? company.companyName() : "null",
-        liked_pets: [],
-        viewed_pets: [],
-        location: address.city(),
-        num_likes: 0
-    };
-    //console.log(user);
-    return user;
-}
-
-function createFakeLogin(){
-    let login = {
-        username: internet.userName(),
-        password: internet.password(), 
-    };
-    return login;
-}
-
-function createFakeLoginToken(){
-    return internet.password(); //using this as a placeholder
-}
-
-function createFakePetResult(type,query){
-    let pet = {
-        pet_name: name.firstName(),
-        pet_location: company.companyName(),
-        pet_breed: commerce.color(),
-        pet_about: lorem.sentence(5,10),
-        pet_health: lorem.sentence(5,10),
-        pet_comments: [],
-        picture: image.cats(),
-        num_likes: 0
-    };
-    let fields = Object.keys(pet);
-    if(fields.includes(type)){ //guarantees that the fake data satisfies the search constraints
-        pet[type] = query;
-    }
-    return pet;
-}
-
-function createFakeSearchResult(type,query,quantity){
-    let shelter_fields = Object.keys(createFakeShelterResult("null",""));
-    let pet_fields = Object.keys(createFakePetResult("null",""));
-    let returnArr = [];
-
-    if(pet_fields.includes(type)){
-        for(let i = 0;i<parseInt(quantity);i++){
-            returnArr.push(createFakePetResult(type,query));
-        }
-    }
-    else if(shelter_fields.includes(type)){
-        for(let i = 0;i<parseInt(quantity);i++){
-            returnArr.push(createFakeShelterResult(type,query));
-        }
-    }
-    else{
-        console.log("Invalid query type");
-    }
-    return returnArr;
-}
-
-//database.users.push(createFakeUser("Eric")); //used for testing user/id/edit
-    
-function createFakePet(pet_name) {
-    //Pet Objects: Name, Breed, About, Health, Location, Comments, Num Likes
-    let i;
-    const commarr = [];
-    const num_com = Math.random() * 10;
-    for (i = 0; i < num_com; i++) {
-        commarr.push({comment: lorem.sentence(), user: internet.userName()});
-    }
-    
-    const pet = {
-        name:  pet_name,
-        breed: 'terrier',
-        about: lorem.paragraph(),
-        health: lorem.paragraph(),
-        location: company.companyName(),
-        comments: commarr,
-        num_likes: Math.floor(Math.random() * 100),
-        picture: image.cats()
-    };
-    return pet;
-}
-
-function createFakeShelter(shelter_name) {
-    //Shelter Objects: Shelter Name, About, Pets, Location, Comments, Num Likes
-    let i;
-    const commarr = [];
-    const num_com = Math.random() * 10;
-    for (i = 0; i < num_com; i++) {
-        commarr.push({comment: lorem.sentence(), user: internet.userName()});
-    }
-
-    const petarr = [];
-    const num_pets = Math.random() * 10;
-    for (i = 0; i < num_pets; i++) {
-        petarr.push(createFakePet(name.firstName()));
-    }
-
-    const shelter = {
-        name: shelter_name,
-        about: lorem.paragraph(),
-        pets: petarr,
-        location: address.city(),
-        comments: commarr,
-        num_likes: Math.floor(Math.random() * 100),
-        picture: image.business()
-    };
-
-    return shelter;
-}
-
-function favoriteShelters(range) {
-    const shelters = [];
-    let i;
-    for (i = 0; i < range; i++) {
-        shelters.push(createFakeShelter(company.companyName()));
-    }
-    return shelters;
-}
-
-function recentlyViewedPets() {
-    const pets = [];
-    let i;
-    for (i = 0; i < 5; i++) {
-        pets.push(createFakePet(name.firstName()));
-    }
-    return pets;
-}
-
-function favoritePets(range) {
-    const pets = [];
-    let i;
-    for (i = 0; i < range; i++) {
-        pets.push(createFakePet(name.firstName()));
-    }
-    return pets;
-}
-
-//Chat Functions
-let chat = [];
-function createFakeChat(){
-    chat = [];
-    for (let i = 0; i < 10; ++i){
-        let fakeName = name.findName();
-        let messages = [];
-        for (let j = 0; j < 5; ++j){
-            let sentence1 = lorem.sentence();
-            let sentence2 = lorem.sentence();
-            messages.push({'key': 0, 'value': sentence1});
-            messages.push({'key': 1, 'value': sentence2});
-        }
-        chat.push({
-            id: i,
-            name: fakeName,
-            messages: messages
-        });
-    }
-    return chat; 
-}
-
-function msgChat(req, res){
-    console.log(req.body.id);
-    console.log(req.body.value);
-    res.send();
-}
-
-//Shelter Functions
-function createFakeShelterResult(type,query){    
-    let shelter = {        
-        shelter_name: company.companyName(),        
-        shelter_location: company.companyName(),        
-        shelter_about: lorem.sentence(5,10),        
-        shelter_pets: null,        
-        shelter_comments: [],        
-        picture: image.cats()    
-    };    
-    let petArr = [];    
-    for(let i = 0; i < 10;i++){       
-        petArr.push(createFakePetResult("location",shelter.shelter_name))   
-    }   
-    shelter.shelter_pets = petArr;   
-    let fields = Object.keys(shelter); 
-    if(fields.includes(type)){ //guarantees that the fake data satisfies the search constraints        
-        shelter[type] = query;    
-    }   
-    return shelter;
-}
-
-//Handles search requests and returns search results (with fake data)
-function search(req,res){
-    let options = req.query;
-    res.end(JSON.stringify(createFakeSearchResult(options.type,options.query,options.quantity)));
-}
-
-//Handles login requests and returns a fake session token(will be replaced in future milestones.)
-function login(req, res){
-    let options = req.body;
-    console.log(options);
-    if((options.username === null) || (options.password === null)){
-        //validates that sufficient information was passed in for the login attempt to occur
-        res.end("Login attempt failed- Invalid request.");
-        return;
-    }
-    else if(Math.random() > 0.5){ //50% Chance of success on login to simulate successful or unsuccessful login attempts
-        //if login succeeds
-        let newToken = createFakeLoginToken();
-        database.tokens.push(newToken);
-        res.end(JSON.stringify(newToken));
-    }
-    else{
-        //if login failed
-        res.end("Login attempt failed- Username and/or Password incorrect.");
-    }
-}
 
 //Edits user data stored on the server.
 // some fields not successfully received
